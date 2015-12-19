af1dbab56555c4a1dff1f19562715ce264b30d03
Vadim Platonov <vadim.platonov@tuloscapital.com>
2015-12-19 22:31:54 +0200

    remove dependency-test project

--- dependency-test/project.clj
+++
@@ -1,4 +0,0 @@
-(defproject test/no-deps "0.1.0-SNAPSHOT"
-  :description "Clojure.Java-Time with no external dependencies"
-  :dependencies [[clojure.java-time "0.1.0-SNAPSHOT"]
-                 [org.clojure/clojure "1.7.0"]])
