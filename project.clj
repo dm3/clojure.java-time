882c07894bb1b75034c89ae90078353ca05f64a4
Vadim Platonov <platonovadim@gmail.com>
2018-04-02 10:56:14 +0200

    Version 0.3.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure.java-time "0.3.2"
+(defproject clojure.java-time "0.3.3-SNAPSHOT"
   :description "Clojure wrapper for Java 8 Time API"
   :url "http://github.com/dm3/clojure.java-time"
   :license {:name "MIT License"
