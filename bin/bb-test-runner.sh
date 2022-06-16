9bc3a96c43ccd8230d0108911a6522cdd6a2e30e
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-06-16 11:29:03 -0400

    fix bb script

--- bin/bb-test-runner.sh
+++ bin/bb-test-runner.sh
@@ -1,20 +1,4 @@
-#!/usr/bin/env bb --debug
-(ns bb-test-runner
-  (:require [clojure.test :as t]
-            [babashka.classpath :as cp]))
+#!/bin/bash
 
-;;TODO
-(def test-nsyms [#_'java-time-test
-                 #_'java-time.graph-test])
-
-(some->> (seq test-nsyms)
-         (apply require))
-
-(def test-results
-  (apply t/run-tests test-nsyms))
-
-(def failures-and-errors
-  (let [{:keys [:fail :error]} test-results]
-    (+ fail error)))
-
-(System/exit failures-and-errors)
+set -e
+bb --debug --file ./bin/bb-test-runner.clj
