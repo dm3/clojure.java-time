01632715a5b0cfbe661d479517bd6c41fdb7672f
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2023-01-05 13:11:10 -0500

    release doc

--- bin/release.sh
+++ bin/release.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+# Releases the current version in project.clj and bump to the next :minor version.
+# Updates changelog and readme to released version.
+# Does not push anything if deployment fails.
+# If deployment fails, reset to origin/master and remove the latest tag to reset local repo.
 lein release :minor
